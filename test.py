6eaa82a5e4da4d29bcf955812df6143b4c45d072
dnyt <lyricaldnyt@gmail.com>
2018-04-01 22:11:23 +0300

    Ran save test and fixed it

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
-from user import User
+from credentials import Credentials
 
-class TestUser(unittest.TestCase):
+class TestCredentials(unittest.TestCase):
     '''
     defines the cases for the contact class behaviour
     '''
@@ -10,7 +10,29 @@ class TestUser(unittest.TestCase):
         '''
         run before each TestCase
         '''
-        self.new_user = User("Dnyt","hd","0710682029","test.user.com")
+        self.new_credentials = Credentials("Dnyt","hd","0710682029","dnyt@moringa.com")
+
+    def test_init(self):
+        '''
+        test if object is initialized
+        '''
+
+        self.assertEqual(self.new_credentials.first_name,"Dnyt")
+        self.assertEqual(self.new_credentials.last_name,"hd")
+        self.assertEqual(self.new_credentials.number,"0710682029")
+        self.assertEqual(self.new_credentials.email,"dnyt@moringa.com")
+
+    def test_save_credentials(self):
+        '''
+        test to save contacts
+        '''
+        self.new_credentials.save_credentials()
+
+        self.assertEqual(len(Credentials.contact_list),1)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
