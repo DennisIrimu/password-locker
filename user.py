696c891eb5c45cb346df8b636262cea1b5f15fdb
dnyt <lyricaldnyt@gmail.com>
2018-04-01 23:21:39 +0300

    Initialized user function with if statement

--- user.py
+++ user.py
@@ -16,4 +16,44 @@ def display_credentials(credentials):
     displays user Credentials
     '''
     return Credentials.display_credentials()
-    
+
+def user():
+    print("We're here to save your credentials and as a bonus we even help with password generation.")
+
+    user_name = input()
+
+    print(f"Hello {user_name}. What are you here to do?")
+    print('\n')
+
+    while True:
+        print("Use the following to navigate through the app: new - create new account,display - display contacts,pg - password generation,exit - if you wanna leave")
+        initials = input().lower()
+
+        if initials =='new':
+            print("Sign up")
+            print("-"*10)
+
+            print("Enter your first name")
+            f_name = input()
+
+            print("Enter your last name")
+            f_name = input()
+
+            print("Enter your mobile phone number")
+            p_number = input()
+
+            print("Enter your email address")
+            e_address = input()
+
+            save_credentials(create_credentials(f_name,l_name,p_number,e_address))
+
+            print ('\n')
+            print(f"New Account {f_name}{l_name} is saved")
+
+        elif initials == 'display':
+
+            if display_credentials():
+                print(f"Here are your currently saved credentials Mrs/Madam {f_name} {l_name}")
+                print('\n')
+                for credentials in display_credentials():
+                    print(f"{credentials.first_name} {credentials.last_name}...{})
